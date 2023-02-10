f67fc008ca9c456ffa564a36e8907c6736ac4ba0
venkatarohank <vkambhampati@equinix.com>
2023-02-10 14:04:45 +0530

    changes convert methods

--- main.py
+++ main.py
@@ -7,27 +7,36 @@ def add_binary(num1,num2):
 
 
 
-def convertPos(n,l):
+def convertPos(n):
+    l = []
     while n!=0:
         l.append(n%2)
-        n=n/2
+        n=n//2
+    l.reverse()
+    return l
 
-def convertNeg(n,lis):
-    convertPos(n,lis)
+def convertNeg(n):
+    lis=convertPos(n*-1)
     for i in range(len(lis)):
         lis[i]=(lis[i]+1)%2
-    t=0
+    t=len(lis)-1
     c=1
     while t!=len(lis) and c==1:
         if lis[t]==0:
             c=0
             lis[t]=1
-        t=t+1        
+        t=t+1  
+    return lis      
 
 
 def main():
-    num1 = int(input("Enter First Number:"))
-    num2 = int((input("Enter Second Number")))
+    num1 =-7 #int(input("Enter First Number:"))
+    num2 = 22#int((input("Enter Second Number")))
     l1=[]
     l2=[]
+    # print(convertPos(num1))
+    print(convertNeg(num1))
 
+    #print(l1)
+
+main()
